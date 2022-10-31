19029893d477d75ccfd200bb87621525720ab1ba
Elzubair22 <elzubairbder@gmail.com>
2022-10-31 07:06:30 -0700

    add all file to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TodoItem from "./components/TodoITem/TodoItem";
+import AddItem from "./components/AddItem/AddItem";
+import './App.css'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state = {
+    items: [
+      { id: 1, name: "alzubair", age: 23 },
+      { id: 2, name: "ali", age: 33 },
+      { id: 3, name: "ahmed", age: 22 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    let items = this.state.items.filter(item => {
+      return item.id !== id
+    })
+    this.setState({items})
+  }
+
+  addItem = (item) => {
+    item.id = Math.random()
+    let items = this.state.items
+    items.push(item)
+    this.setState({items})
+  }
+
+  render() {
+
+    return (
+      <div className="App container">
+        <h1 className="text">Todo List</h1>
+        <TodoItem items={this.state.items} deleteItem={this.deleteItem}/>
+        <AddItem addItem={this.addItem} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
